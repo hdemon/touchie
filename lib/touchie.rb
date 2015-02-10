452352254f2c225948ef388e5131e622148abdfe
Masami Yonehara <z.masami.yonehara@gree.net>
2015-02-10 18:03:50 +0900

    add method to find odd size images

--- lib/touchie.rb
+++ lib/touchie.rb
@@ -1,5 +1,11 @@
 require "touchie/version"
+require "fastimage"
 
 module Touchie
-  # Your code goes here...
+  def self.find_odd_size_images(image_paths)
+    image_paths.map do |image_path|
+      size = FastImage.size(image_path)
+      image_path if !size.nil? && (size[0].odd? || size[1].odd?)
+    end
+  end
 end
