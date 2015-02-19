566ddfeec57c831009a17a751312f65a910b8d8f
Masami Yonehara <z.masami.yonehara@gree.net>
2015-02-19 20:14:44 +0900

    add a method to fix image

--- lib/touchie.rb
+++ lib/touchie.rb
@@ -10,4 +10,21 @@ module Touchie
       image_path if !size.nil? && (size[0].odd? || size[1].odd?)
     end.reject(&:nil?)
   end
+
+  def self.create_transparent_image(x, y)
+    Image.new(x, y) do |canvas|
+      canvas.background_color= "Transparent"
+    end
+  end
+
+  def self.extend_size_to_odd(image)
+    x = image.columns
+    y = image.rows
+
+    x += 1 if x % 2 != 0
+    y += 1 if y % 2 != 0
+
+    base = create_transparent_image x, y
+    base.composite(image, 0, 0, OverCompositeOp)
+  end
 end
