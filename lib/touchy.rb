566913bb4de277170f4a19bf36b2be148a9d2ec0
Masami Yonehara <z.masami.yonehara@gree.net>
2015-02-25 14:21:04 +0900

    remove fastimage dependency

--- lib/touchy.rb
+++ lib/touchy.rb
@@ -6,8 +6,8 @@ include Magick
 module Touchy
   def self.find_odd_size_images(image_paths)
     image_paths.map do |image_path|
-      size = FastImage.size(image_path)
-      image_path if !size.nil? && (size[0].odd? || size[1].odd?)
+      image = Magick::Image.read(image_path).first
+      image_path if (image.columns && image.rows) && (image.columns.odd? || image.rows.odd?)
     end.reject(&:nil?)
   end
 
--- lib/touchy/version.rb
+++ lib/touchy/version.rb
@@ -1,3 +1,3 @@
 module Touchy
-  VERSION = "0.1.0"
+  VERSION = "0.1.1"
 end
